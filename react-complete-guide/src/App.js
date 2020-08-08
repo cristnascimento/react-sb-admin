d8a86587021e25e50468b9e402865515796ae8ca
Cristiano Nascimento <cristnascimento@gmail.com>
2020-08-07 21:33:59 -0300

    Using state. PS: note that this version of react use function instead class for App

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Person from './Person/Person'
 
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    person: [
+      {name:"John", age: 28},
+      {name:"Mary", age: 31},
+      {name:"Sam", age: 35}
+    ]
+  };
+
+  render () {
+    return (
     <div className="App">
      <h1>Hello React!</h1>
-     <Person name="John" age="28"/>
-     <Person name="Mary" age="31"> This is my biography </Person>
-     <Person name="Sam" age="35"/>
+     <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+     <Person name={this.state.person[1].name} age={this.state.person[1].age}> This is my biography </Person>
+     <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
