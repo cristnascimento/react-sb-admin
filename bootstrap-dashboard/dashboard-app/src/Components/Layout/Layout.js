0108433ddf3a3914c4a7efc6f89869a1319b3ecc
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-13 19:29:50 -0300

    Adding SideBar. closes #24

--- bootstrap-dashboard/dashboard-app/src/Components/Layout/Layout.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Layout/Layout.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import SideBar from './SideBar/SideBar';
+
 const Layout = (props) => {
     return (
-        <div>{props.children}</div>
+        <div  id="wrapper">
+            <SideBar/>
+            {props.children}
+        </div>
     );
 }
 
