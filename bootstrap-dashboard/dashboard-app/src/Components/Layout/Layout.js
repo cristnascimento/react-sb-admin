1fd946291fead3179cdee8717d8a787999432027
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-14 22:37:23 -0300

    Adding a ContentWrapper. closes #27

--- bootstrap-dashboard/dashboard-app/src/Components/Layout/Layout.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Layout/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import ContentWrapper from './MyContentWrapper';
 
 import SideBar from './SideBar/SideBar';
+import MyContentWrapper from './MyContentWrapper';
 
 const Layout = (props) => {
     return (
         <div  id="wrapper">
             <SideBar/>
-            {props.children}
+            <MyContentWrapper>
+                {props.children}
+            </MyContentWrapper>
         </div>
     );
 }
---
+++ bootstrap-dashboard/dashboard-app/src/Components/Layout/MyContentWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyContentWrapper = (props) => {
+    return (
+        <div>{props.children}</div>
+    );
+}
+
+export default MyContentWrapper;
