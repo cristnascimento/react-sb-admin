c6b482b2adc9a51d359530867917de7ef2d08cb4
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-17 22:01:28 -0300

    Adding TopBar Component. closes #31

--- bootstrap-dashboard/dashboard-app/src/Components/Layout/Layout.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Layout/Layout.js
@@ -4,14 +4,17 @@ import ContentWrapper from './MyContentWrapper';
 import SideBar from './SideBar/SideBar';
 import MyContentWrapper from './MyContentWrapper';
 import Content from './Content';
+import TopBar from './TopBar';
 
 const Layout = (props) => {
     return (
         <div  id="wrapper">
             <SideBar/>
             <MyContentWrapper>
-                <Content/>
-                {props.children}
+                <Content>
+                    <TopBar/>
+                    {props.children}
+                </Content>
             </MyContentWrapper>
         </div>
     );
