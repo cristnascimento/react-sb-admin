44e82551d480ae5128e847f5a7642540b2e1e3ee
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-22 20:36:01 -0300

    Adding AreaChart for Dashboard. closes #49

--- bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
@@ -1,18 +1,18 @@
 import React, {useRef, useEffect} from 'react';
 import Chart from 'chart.js'
-import fillArea from './chart-area-demo';
+
 import fillPie from './chart-pie-demo';
 import PageHeading from './PageHeading';
 import EarningsMonthly from './EarningsMonthly';
 import EarningsAnnual from './EarningsAnnual';
 import Tasks from './Tasks';
 import PendingRequests from './PendingRequests';
+import AreaChart from './AreaChart';
 
 const Dashboard = (props) => {
 
     const chartContainer = useRef(null);
-    const chartContainerArea = useRef(null);
-
+    
     useEffect(() => {
         if (chartContainer && chartContainer.current) {
             fillPie(chartContainer);
@@ -20,14 +20,6 @@ const Dashboard = (props) => {
           //setChartInstance(newChartInstance);
         }
       }, [chartContainer]);
-
-    useEffect(() => {
-        if (chartContainerArea && chartContainerArea.current) {
-            fillArea(chartContainerArea);
-        //setChartInstance(newChartInstance);
-        }
-    }, [chartContainerArea]);
-
 
     return (
         <div className="container-fluid">
@@ -48,32 +40,7 @@ const Dashboard = (props) => {
           <div className="row">
 
             {/* Area Chart */}
-            <div className="col-xl-8 col-lg-7">
-              <div className="card shadow mb-4">
-                {/* Card Header - Dropdown */}
-                <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                  <div className="dropdown no-arrow">
-                    <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                      <div className="dropdown-header">Dropdown Header:</div>
-                      <a className="dropdown-item" href="#">Action</a>
-                      <a className="dropdown-item" href="#">Another action</a>
-                      <div className="dropdown-divider"></div>
-                      <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                  </div>
-                </div>
-                {/* Card Body */}
-                <div className="card-body">
-                  <div className="chart-area">
-                    <canvas id="myAreaChart" ref={chartContainerArea}></canvas>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <AreaChart/>
 
             {/* Pie Chart */}
             <div className="col-xl-4 col-lg-5">
