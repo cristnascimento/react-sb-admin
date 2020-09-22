1cccf1d266df0616c0d180f50492fb63379e2f97
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-21 22:05:44 -0300

    Adding EarningsMontlhy for Dashboard. closes #45

--- bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import Chart from 'chart.js'
 import fillArea from './chart-area-demo';
 import fillPie from './chart-pie-demo';
 import PageHeading from './PageHeading';
+import EarningsMonthly from './EarningsMonthly';
 
 const Dashboard = (props) => {
 
@@ -36,22 +37,8 @@ const Dashboard = (props) => {
           <div className="row">
 
             {/* Earnings (Monthly) Card Example */}
-            <div className="col-xl-3 col-md-6 mb-4">
-              <div className="card border-left-primary shadow h-100 py-2">
-                <div className="card-body">
-                  <div className="row no-gutters align-items-center">
-                    <div className="col mr-2">
-                      <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                      <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                    </div>
-                    <div className="col-auto">
-                      <i className="fas fa-calendar fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
+            <EarningsMonthly/>
+            
             {/* Earnings (Monthly) Card Example */}
             <div className="col-xl-3 col-md-6 mb-4">
               <div className="card border-left-success shadow h-100 py-2">
