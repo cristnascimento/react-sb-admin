c4564ef38cbd627ee680888074d39f995090efa5
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-22 20:19:31 -0300

    Adding EarninsgAnnual for Dashboard. closes #46

--- bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import fillArea from './chart-area-demo';
 import fillPie from './chart-pie-demo';
 import PageHeading from './PageHeading';
 import EarningsMonthly from './EarningsMonthly';
+import EarningsAnnual from './EarningsAnnual';
 
 const Dashboard = (props) => {
 
@@ -38,24 +39,9 @@ const Dashboard = (props) => {
 
             {/* Earnings (Monthly) Card Example */}
             <EarningsMonthly/>
-            
-            {/* Earnings (Monthly) Card Example */}
-            <div className="col-xl-3 col-md-6 mb-4">
-              <div className="card border-left-success shadow h-100 py-2">
-                <div className="card-body">
-                  <div className="row no-gutters align-items-center">
-                    <div className="col mr-2">
-                      <div className="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                      <div className="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                    </div>
-                    <div className="col-auto">
-                      <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
+            {/* Earnings (Annual) Card Example */}
+            <EarningsAnnual/>
             {/* Earnings (Monthly) Card Example */}
             <div className="col-xl-3 col-md-6 mb-4">
               <div className="card border-left-info shadow h-100 py-2">
