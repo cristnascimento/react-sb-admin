41f334358f4e1008641c33d022b0c5616d30da06
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-23 20:35:40 -0300

    Adding Approach for Dashboard. closes #54

--- bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Dashboard/Dashboard.js
@@ -10,6 +10,7 @@ import PieChart from './PieChart';
 import Projects from './Projects';
 import ColorSystem from './ColorSystem';
 import Illustrations from './Illustrations';
+import Approach from './Approach';
 
 const Dashboard = (props) => {
 
@@ -38,28 +39,14 @@ const Dashboard = (props) => {
           {/* Content Row */}
           <div className="row">
 
-            {/* Content Column */}
             <div className="col-lg-6 mb-4">
               <Projects/>
               <ColorSystem/>
             </div>
 
             <div className="col-lg-6 mb-4">
-
-              {/* Illustrations */}
               <Illustrations/>
-
-              {/* Approach */}
-              <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                  <h6 className="m-0 font-weight-bold text-primary">Development Approach</h6>
-                </div>
-                <div className="card-body">
-                  <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>
-                  <p className="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>
-                </div>
-              </div>
-
+              <Approach/>  
             </div>
           </div>
       
