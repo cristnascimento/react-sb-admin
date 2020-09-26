722ac9d04c7464fe9999db8043971c746b621a2d
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-26 18:58:05 -0300

    Adding SplitButtons. closes #60

--- bootstrap-dashboard/dashboard-app/src/Components/Components/Buttons.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Components/Buttons.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BrandButtons from './BrandButtons';
 import CircleButtons from './CircleButtons';
+import SplitButtons from './SplitButtons';
 
 const Buttons = (props) => {
     return (
@@ -11,86 +12,13 @@ const Buttons = (props) => {
         <h1 className="h3 mb-4 text-gray-800">Buttons</h1>
 
         <div className="row">
-
           <div className="col-lg-6">
             <CircleButtons/>
             <BrandButtons/>
           </div>
 
           <div className="col-lg-6">
-
-            <div className="card shadow mb-4">
-              <div className="card-header py-3">
-                <h6 className="m-0 font-weight-bold text-primary">Split Buttons with Icon</h6>
-              </div>
-              <div className="card-body">
-                <p>Works with any button colors, just use the <code>.btn-icon-split</code> class and the markup in the examples below. The examples below also use the <code>.text-white-50</code> helper class on the icons for additional styling, but it is not required.</p>
-                <a href="#" className="btn btn-primary btn-icon-split">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-flag"></i>
-                  </span>
-                  <span className="text">Split Button Primary</span>
-                </a>
-                <div className="my-2"></div>
-                <a href="#" className="btn btn-success btn-icon-split">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-check"></i>
-                  </span>
-                  <span className="text">Split Button Success</span>
-                </a>
-                <div className="my-2"></div>
-                <a href="#" className="btn btn-info btn-icon-split">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-info-circle"></i>
-                  </span>
-                  <span className="text">Split Button Info</span>
-                </a>
-                <div className="my-2"></div>
-                <a href="#" className="btn btn-warning btn-icon-split">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-exclamation-triangle"></i>
-                  </span>
-                  <span className="text">Split Button Warning</span>
-                </a>
-                <div className="my-2"></div>
-                <a href="#" className="btn btn-danger btn-icon-split">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-trash"></i>
-                  </span>
-                  <span className="text">Split Button Danger</span>
-                </a>
-                <div className="my-2"></div>
-                <a href="#" className="btn btn-secondary btn-icon-split">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-arrow-right"></i>
-                  </span>
-                  <span className="text">Split Button Secondary</span>
-                </a>
-                <div className="my-2"></div>
-                <a href="#" className="btn btn-light btn-icon-split">
-                  <span className="icon text-gray-600">
-                    <i className="fas fa-arrow-right"></i>
-                  </span>
-                  <span className="text">Split Button Light</span>
-                </a>
-                <div className="mb-4"></div>
-                <p>Also works with small and large button classes!</p>
-                <a href="#" className="btn btn-primary btn-icon-split btn-sm">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-flag"></i>
-                  </span>
-                  <span className="text">Split Button Small</span>
-                </a>
-                <div className="my-2"></div>
-                <a href="#" className="btn btn-primary btn-icon-split btn-lg">
-                  <span className="icon text-white-50">
-                    <i className="fas fa-flag"></i>
-                  </span>
-                  <span className="text">Split Button Large</span>
-                </a>
-              </div>
-            </div>
-
+            <SplitButtons/>
           </div>
 
         </div>
