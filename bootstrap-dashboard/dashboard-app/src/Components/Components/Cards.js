371cd72649740d6a814cf25f59402535a6131be8
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-26 20:37:11 -0300

    Adding DropdownCard. closes #63

--- bootstrap-dashboard/dashboard-app/src/Components/Components/Cards.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Components/Cards.js
@@ -8,6 +8,7 @@ import Tasks from '../Dashboard/Tasks';
 import PendingRequests from '../Dashboard/PendingRequests';
 import DefaultCard from './DefaultCard';
 import BasicCard from './BasicCard';
+import DropdownCard from './DropdownCard';
 
 const Cards = (props) => {
     return (
@@ -30,29 +31,7 @@ const Cards = (props) => {
 
           <div className="col-lg-6">
 
-            {/* Dropdown Card Example */}
-            <div className="card shadow mb-4">
-              {/* Card Header - Dropdown */}
-              <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 className="m-0 font-weight-bold text-primary">Dropdown Card Example</h6>
-                <div className="dropdown no-arrow">
-                  <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                  </a>
-                  <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                    <div className="dropdown-header">Dropdown Header:</div>
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </div>
-              </div>
-              {/* Card Body */}
-              <div className="card-body">
-                Dropdown menus can be placed in the card header in order to extend the functionality of a basic card. In this dropdown card example, the Font Awesome vertical ellipsis icon in the card header can be clicked on in order to toggle a dropdown menu.
-              </div>
-            </div>
+            <DropdownCard/>
 
             {/* Collapsable Card Example */}
             <div className="card shadow mb-4">
