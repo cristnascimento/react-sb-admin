c0496d4da845e938ed2638eab9c73d349fafa0f2
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-30 20:56:22 -0300

    Adding Grayscale. closes #68

--- bootstrap-dashboard/dashboard-app/src/Components/Utilities/Colors.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Utilities/Colors.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BackgroundGradient from './BackgroundGradient';
 import FontSize from './FontSize';
+import Grayscale from './Grayscale';
 import TextColor from './TextColor';
 
 const Colors = (props) => {
@@ -23,32 +24,14 @@ const Colors = (props) => {
 
             {/* Second Column */}
             <div className="col-lg-4">
-
               <BackgroundGradient/>
-
             </div>
 
             {/* Third Column */}
             <div className="col-lg-4">
-
-              {/* Grayscale Utilities */}
-              <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                  <h6 className="m-0 font-weight-bold text-primary">Custom Grayscale Background Utilities</h6>
-                </div>
-                <div className="card-body">
-                  <div className="p-3 bg-gray-100">.bg-gray-100</div>
-                  <div className="p-3 bg-gray-200">.bg-gray-200</div>
-                  <div className="p-3 bg-gray-300">.bg-gray-300</div>
-                  <div className="p-3 bg-gray-400">.bg-gray-400</div>
-                  <div className="p-3 bg-gray-500 text-white">.bg-gray-500</div>
-                  <div className="p-3 bg-gray-600 text-white">.bg-gray-600</div>
-                  <div className="p-3 bg-gray-700 text-white">.bg-gray-700</div>
-                  <div className="p-3 bg-gray-800 text-white">.bg-gray-800</div>
-                  <div className="p-3 bg-gray-900 text-white">.bg-gray-900</div>
-                </div>
-              </div>
+              <Grayscale/>              
             </div>
+
           </div>
       {/* /.container-fluid */}
       </div>
