4da58fa000153d6626862ff837c25ea2c73f5dec
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-03 12:17:41 -0300

    Adding FadeIn to Animations. closes #72

--- bootstrap-dashboard/dashboard-app/src/Components/Utilities/Animations.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Utilities/Animations.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FadeIn from './FadeIn';
 import GrownIn from './GrownIn';
 
 const Animations = (props) => {
@@ -14,53 +15,12 @@ const Animations = (props) => {
 
           {/* Grow In Utility */}
           <div className="col-lg-6">
-
-          <GrownIn/>
+            <GrownIn/>
           </div>
 
           {/* Fade In Utility */}
           <div className="col-lg-6">
-
-            <div className="card position-relative">
-              <div className="card-header py-3">
-                <h6 className="m-0 font-weight-bold text-primary">Fade In Animation Utilty</h6>
-              </div>
-              <div className="card-body">
-                <div className="mb-3">
-                  <code>.animated--fade-in</code>
-                </div>
-                <div className="small mb-1">Navbar Dropdown Example:</div>
-                <nav className="navbar navbar-expand navbar-light bg-light mb-4">
-                  <a className="navbar-brand" href="#">Navbar</a>
-                  <ul className="navbar-nav ml-auto">
-                    <li className="nav-item dropdown">
-                      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                      </a>
-                      <div className="dropdown-menu dropdown-menu-right animated--fade-in" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                      </div>
-                    </li>
-                  </ul>
-                </nav>
-                <div className="small mb-1">Dropdown Button Example:</div>
-                <div className="dropdown mb-4">
-                  <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </button>
-                  <div className="dropdown-menu animated--fade-in" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </div>
-                <p className="mb-0 small">Note: This utility animates the CSS opacity property, meaning it will override any existing opacity on an element being animated!</p>
-              </div>
-            </div>
-
+            <FadeIn/>
           </div>
 
         </div>
