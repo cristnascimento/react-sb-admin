f96d840c824f67e5465e4af45ba3856cdd47eb5f
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-03 12:27:24 -0300

    Adding Progress to Other. closes #74

--- bootstrap-dashboard/dashboard-app/src/Components/Utilities/Other.js
+++ bootstrap-dashboard/dashboard-app/src/Components/Utilities/Other.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Overflow from './Overflow';
+import Progress from './Progress';
 
 const Other = (props) => {
     return (
@@ -15,25 +16,8 @@ const Other = (props) => {
 
           <div className="col-lg-6">
             <Overflow/> 
-
-            {/* Progress Small */}
-            <div className="card mb-4">
-              <div className="card-header py-3">
-                <h6 className="m-0 font-weight-bold text-primary">Progress Small Utility</h6>
-              </div>
-              <div className="card-body">
-                <div className="mb-1 small">Normal Progress Bar</div>
-                <div className="progress mb-4">
-                  <div className="progress-bar" role="progressbar" style={{width: '75%'}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <div className="mb-1 small">Small Progress Bar</div>
-                <div className="progress progress-sm mb-2">
-                  <div className="progress-bar" role="progressbar" style={{width: '75%'}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                Use the <code>.progress-sm</code> class along with <code>.progress</code>
-              </div>
-            </div>
-
+            <Progress/>
+          
             {/* Dropdown No Arrow */}
             <div className="card mb-4">
               <div className="card-header py-3">
---
+++ bootstrap-dashboard/dashboard-app/src/Components/Utilities/Progress.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Progress = (props) => {
+    return (
+        <div className="card mb-4">
+        {/* Progress Small */}
+        <div className="card-header py-3">
+            <h6 className="m-0 font-weight-bold text-primary">Progress Small Utility</h6>
+        </div>
+        <div className="card-body">
+            <div className="mb-1 small">Normal Progress Bar</div>
+            <div className="progress mb-4">
+            <div className="progress-bar" role="progressbar" style={{width: '75%'}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div className="mb-1 small">Small Progress Bar</div>
+            <div className="progress progress-sm mb-2">
+            <div className="progress-bar" role="progressbar" style={{width: '75%'}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            Use the <code>.progress-sm</code> class along with <code>.progress</code>
+        </div>
+        </div>
+    );
+}
+
+export default Progress;
