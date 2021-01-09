7e1f6789744d0a3d922691b395d17f92d7e2d240
Cristiano Nascimento <cristnascimento@gmail.com>
2021-01-09 19:07:54 -0300

    Adding Profile Form Controls. closes #21

--- dashboard-app/src/Components/Profile/Contact.js
+++ dashboard-app/src/Components/Profile/Contact.js
@@ -2,72 +2,34 @@ import React from 'react';
 
 const Contact = (props) => {
     return (
- <div className="card shadow mb-4">
-      {/* Circle Buttons */}
- <div className="card-header py-3">
-   <h6 className="m-0 font-weight-bold text-primary">Circle Buttons</h6>
- </div>
- <div className="card-body">
-   <p>Use Font Awesome Icons (included with this theme package) along with the circle buttons as shown in the examples below!</p>
-   {/* Circle Buttons (Default) */}
-   <div className="mb-2">
-     <code>.btn-circle</code>
-   </div>
-   <a href="#" className="btn btn-primary btn-circle">
-     <i className="fab fa-facebook-f"></i>
-   </a>
-   <a href="#" className="btn btn-success btn-circle">
-     <i className="fas fa-check"></i>
-   </a>
-   <a href="#" className="btn btn-info btn-circle">
-     <i className="fas fa-info-circle"></i>
-   </a>
-   <a href="#" className="btn btn-warning btn-circle">
-     <i className="fas fa-exclamation-triangle"></i>
-   </a>
-   <a href="#" className="btn btn-danger btn-circle">
-     <i className="fas fa-trash"></i>
-   </a>
-   {/* Circle Buttons (Small) */}
-   <div className="mt-4 mb-2">
-     <code>.btn-circle .btn-sm</code>
-   </div>
-   <a href="#" className="btn btn-primary btn-circle btn-sm">
-     <i className="fab fa-facebook-f"></i>
-   </a>
-   <a href="#" className="btn btn-success btn-circle btn-sm">
-     <i className="fas fa-check"></i>
-   </a>
-   <a href="#" className="btn btn-info btn-circle btn-sm">
-     <i className="fas fa-info-circle"></i>
-   </a>
-   <a href="#" className="btn btn-warning btn-circle btn-sm">
-     <i className="fas fa-exclamation-triangle"></i>
-   </a>
-   <a href="#" className="btn btn-danger btn-circle btn-sm">
-     <i className="fas fa-trash"></i>
-   </a>
-   {/* Circle Buttons (Large) */}
-   <div className="mt-4 mb-2">
-     <code>.btn-circle .btn-lg</code>
-   </div>
-   <a href="#" className="btn btn-primary btn-circle btn-lg">
-     <i className="fab fa-facebook-f"></i>
-   </a>
-   <a href="#" className="btn btn-success btn-circle btn-lg">
-     <i className="fas fa-check"></i>
-   </a>
-   <a href="#" className="btn btn-info btn-circle btn-lg">
-     <i className="fas fa-info-circle"></i>
-   </a>
-   <a href="#" className="btn btn-warning btn-circle btn-lg">
-     <i className="fas fa-exclamation-triangle"></i>
-   </a>
-   <a href="#" className="btn btn-danger btn-circle btn-lg">
-     <i className="fas fa-trash"></i>
-   </a>
- </div>
-</div>
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+              <h6 className="m-0 font-weight-bold text-primary">Circle Buttons</h6>
+          </div>
+          <div className="card-body">
+            <p>Use Font Awesome Icons (included with this theme package) along with the circle buttons as shown in the examples below!</p>
+   
+            <form className="user" >
+            <div className="form-group row">
+                      <div className="col-sm-6 mb-3 mb-sm-0">
+                        <input type="text" className="form-control form-control-user" id="exampleInputPassword" placeholder="First Name"/>
+                      </div>
+                      <div className="col-sm-6">
+                        <input type="text" className="form-control form-control-user" id="exampleRepeatPassword" placeholder="Last Name"/>
+                      </div>
+                    </div>
+                        <div className="form-group">
+                          <input type="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..."/>
+                        </div>
+                        <div className="form-group">
+                          <input type="text" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Phone Number +55 31 99988-7755..."/>
+                        </div>
+                        <button className="btn btn-primary btn-user btn-block">
+                          Reset Password
+                        </button>
+            </form>
+          </div>
+        </div>
     );
 }
 
