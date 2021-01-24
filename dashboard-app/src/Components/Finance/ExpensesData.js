9651ed48ad2da64703accb4a8ceb143b6aa981ac
Cristiano Nascimento <cristnascimento@gmail.com>
2021-01-23 21:41:14 -0300

    Adding mock data for expenses. closes #38

---
+++ dashboard-app/src/Components/Finance/ExpensesData.js
@@ -0,0 +1,48 @@
+export let expensesData = [
+    {
+      id: 1,
+      name: 'Tiger Nixon 1',
+      position: 'System Architect',
+      office: 'Edinburgh',
+      age: '61',
+      startDate: '2011/04/25',
+      salary: '$320,800'
+    },
+    {
+        id: 2,
+        name: 'Tiger Nixon 2',
+        position: 'System Architect',
+        office: 'Edinburgh',
+        age: '61',
+        startDate: '2011/04/25',
+        salary: '$320,800'
+    },
+    {
+        id: 3,
+        name: 'Tiger Nixon 3',
+        position: 'System Architect',
+        office: 'Edinburgh',
+        age: '61',
+        startDate: '2011/04/25',
+        salary: '$320,800'
+    },
+    {
+        id: 4,
+        name: 'Tiger Nixon 4',
+        position: 'System Architect',
+        office: 'Edinburgh',
+        age: '61',
+        startDate: '2011/04/25',
+        salary: '$320,800'
+    },
+    {
+        id: 5,
+        name: 'Tiger Nixon 5',
+        position: 'System Architect',
+        office: 'Edinburgh',
+        age: '61',
+        startDate: '2011/04/25',
+        salary: '$320,800'
+    }
+  
+  ]
